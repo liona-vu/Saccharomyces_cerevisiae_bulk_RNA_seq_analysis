65418f4d9b47e2bba5cb208f48ac9b4c0550ea3d
liona-vu <230396061+liona-vu@users.noreply.github.com>
2026-02-20 22:58:09 -0500

    Create download_script.sh

---
+++ script/download_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Based on the tutorial below:
+#https://combine-lab.github.io/salmon/getting_started/
+
+mkdir raw_data
+cd raw_data
+
+for i in `seq 57 65`; 
+do
+  mkdir SRR105516${i};
+  cd SRR105516${i};
+  wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR105/0${i}/SRR105516${i}/SRR105516${i}.fastq.gz
+  cd ..;
+done
+
+cd ..
