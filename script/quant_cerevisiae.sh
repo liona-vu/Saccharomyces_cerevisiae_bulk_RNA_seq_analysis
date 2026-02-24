341d771fc50b3f5eab1085cc4148ac08a3490b1c
liona-vu <230396061+liona-vu@users.noreply.github.com>
2026-02-24 16:32:55 -0500

    Update quant_cerevisiae.sh
    added the salmon indexing

--- script/quant_cerevisiae.sh
+++ script/quant_cerevisiae.sh
@@ -2,12 +2,19 @@
 # Script used to quantify reads with Salmon
 # Based on tutorial: https://combine-lab.github.io/salmon/getting_started/
 
+# Build index with Salmon first
+salmon index -t ncbi_dataset/ncbi_dataset/data/GCF_000146045.2/rna.fna -i scerevisiae_index
+
+#then run this loop to quantify 
+salmon index -t ncbi_dataset/ncbi_dataset/data/GCF_000146045.2/rna.fna -i scerevisiae_index
 for fn in raw_data/SRR105516{57..65};
 do
 samp=`basename ${fn}`
 #echo "***Processing sample ${samp}***"
 salmon quant -i scerevisiae_index -l A \
 	-r ${fn}/${samp}.fastq.gz \
-	--gcBias \
+	--gcBias \ #recommended by DESeq2 vignette
 	-p 6 --validateMappings -o quants/${samp}
 done
+
+
